1953aff4a3a7e5c7e3eb9ab6d201b80aba961958
Amirali D Shiraz <amiralidshiraz@gmail.com>
2026-06-02 13:15:14 -0400

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+
+print("arguments", sys.argv)
+
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+print(df)
+
+day = int(sys.argv[1])
+print(f"Running pipeline for day {day}")
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
